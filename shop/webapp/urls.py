668a8f6548552b098cd1d0fd66848502f1cce032
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-07 18:23:51 +0600

    create product_detail.html and respectively view and urls/path

--- shop/webapp/urls.py
+++ shop/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index_view
+from .views import index_view, detail_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('detail/<int:pk>', detail_view, name='detail_view')
 ]
--- shop/webapp/views.py
+++ shop/webapp/views.py
@@ -8,3 +8,9 @@ def index_view(request):
     products = Product.objects.all()
     products = {'products': products}
     return render(request, "index.html", context=products)
+
+
+def detail_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
+
