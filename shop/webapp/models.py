08377b47a2651aebe96ad4c63ba5ced8ca447846
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-07 16:34:02 +0600

    create site example and models.py Product

--- shop/webapp/models.py
+++ shop/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+CHOOSE_CATEGORY = [('other', 'разное'), ('clothes', 'одежда'), ('equipment', 'оборудование'), ('information', 'информативные')]
+
+
+class Product(models.Model):
+    product = models.CharField(max_length=100, null=False, blank=False, verbose_name="название")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="описание")
+    category = models.CharField(max_length=100, null=False, blank=False, choices=CHOOSE_CATEGORY, verbose_name="категории")
+    balance = models.PositiveIntegerField(null=False, blank=False, verbose_name="остаток")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="цена")
