57a0926865a07077590df65c3cbb8075ef3ca75b
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-10 16:25:20 +0600

    improvement of choices look way in forms.py

--- shop/webapp/forms.py
+++ shop/webapp/forms.py
@@ -6,7 +6,7 @@ from .models import CHOOSE_CATEGORY
 class ProductForm(forms.Form):
     product = forms.CharField(max_length=100, required=True, label="Название")
     category = forms.ChoiceField(choices=CHOOSE_CATEGORY, required=True,
-                                 label='Категория', initial=CHOOSE_CATEGORY[0][1])
+                                 label='Категория', initial=CHOOSE_CATEGORY[0])
     description = forms.CharField(max_length=2000, required=False, label='Описание',
                                   widget=forms.Textarea)
     balance = forms.IntegerField(required=True, label='остаток', min_value=0)
--- shop/webapp/models.py
+++ shop/webapp/models.py
@@ -8,6 +8,6 @@ class Product(models.Model):
     product = models.CharField(max_length=100, null=False, blank=False, verbose_name="название")
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="описание")
     category = models.CharField(max_length=100, null=False, blank=False, choices=CHOOSE_CATEGORY,
-                                verbose_name="категории", default=CHOOSE_CATEGORY[0][0])
+                                verbose_name="категории", default=CHOOSE_CATEGORY[0])
     balance = models.PositiveIntegerField(null=False, blank=False, verbose_name="остаток")
-    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="цена")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="цена", null=False, blank=False)
